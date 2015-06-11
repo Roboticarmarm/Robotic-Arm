f729f027ed127944e67b1c6ff5a947c314e3b08c
wyc <yen.chen.wu.wyc@gmail.com>
2015-06-11 11:45:20 +0800

    add keyword

--- run-scripts/run.sh
+++ run-scripts/run.sh
@@ -1,16 +1,25 @@
 #arecord -q -c 1 -f S16_LE -r8000 test.wav
 #./sirius-asr-test.sh ./test.wav
 
-for i in {1..1}
+for i in {1..10}
 do
   echo $i
-  arecord -q -d 5 -c 1 -f S16_LE -r8000 test.wav
+  arecord -q -d 3 -c 1 -f S16_LE -r8000 test.wav
   ./sirius-asr-test.sh ./test.wav > histry
-  cat histry
-  if grep -q yes histry
-  then
+  tail -n 2 histry
+
+  if grep -q yes histry;then
     echo "YES!!!"
+  elif grep -q no histry;then
+    echo "NO!!!"
+  elif grep -q move histry;then
+    echo "MOVE!!!"
+  elif grep -q back histry;then
+    echo "back!!!"
   fi
+
+
+
 done
 
 
