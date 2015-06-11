500f602463aebb40f6284fe6318fffad0d22baf4
wyc <yen.chen.wu.wyc@gmail.com>
2015-06-11 11:32:39 +0800

    seq-record & grep keyword

--- run-scripts/run.sh
+++ run-scripts/run.sh
@@ -1,3 +1,16 @@
-arecord -q -c 1 -f S16_LE -r8000 test.wav
-./sirius-asr-test.sh ./test.wav
+#arecord -q -c 1 -f S16_LE -r8000 test.wav
+#./sirius-asr-test.sh ./test.wav
+
+for i in {1..1}
+do
+  echo $i
+  arecord -q -d 5 -c 1 -f S16_LE -r8000 test.wav
+  ./sirius-asr-test.sh ./test.wav > histry
+  cat histry
+  if grep -q yes histry
+  then
+    echo "YES!!!"
+  fi
+done
+
 
