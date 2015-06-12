351e5cb240f77f09edec8e01d05cfb93fedac6d3
mopplayer <mopplayer@yahoo.com.tw>
2015-06-12 01:49:30 +0000

    task of the week

--- mainarm.sh
+++ mainarm.sh
@@ -5,7 +5,7 @@ sudo chmod 777 /dev/ttyACM0
 if [ "$?" == "0" ]; then
 	echo "Succeeded!"
 	case $1 in
-	"-hold")
+	"-hold -9")
 		sudo echo "90,90,90,90,90,90,90,90,90,90,90,90,90,90,90" >> /dev/ttyACM0
 		if [ "$?" == "0" ]; then
 			echo "...done."
@@ -13,10 +13,83 @@ if [ "$?" == "0" ]; then
 			echo "Write error!"
 		fi
          	;;
+	"-hold -8")
+		sudo echo "80,80,80,80,80,80,80,80,80,80,80,80,80,80,80" >> /dev/ttyACM0
+		if [ "$?" == "0" ]; then
+			echo "...done."
+		else
+			echo "Write error!"
+		fi
+         	;;
+	"-hold -7")
+		sudo echo "70,70,70,70,70,70,70,70,70,70,70,70,70,70,70" >> /dev/ttyACM0
+		if [ "$?" == "0" ]; then
+			echo "...done."
+		else
+			echo "Write error!"
+		fi
+         	;;
+	"-hold -6")
+		sudo echo "60,60,60,60,60,60,60,60,60,60,60,60,60,60,60" >> /dev/ttyACM0
+		if [ "$?" == "0" ]; then
+			echo "...done."
+		else
+			echo "Write error!"
+		fi
+         	;;
+	"-hold -5")
+		sudo echo "50,50,50,50,50,50,50,50,50,50,50,50,50,50,50" >> /dev/ttyACM0
+		if [ "$?" == "0" ]; then
+			echo "...done."
+		else
+			echo "Write error!"
+		fi
+         	;;
+	"-hold -4")
+		sudo echo "40,40,40,40,40,40,40,40,40,40,40,40,40,40,40" >> /dev/ttyACM0
+		if [ "$?" == "0" ]; then
+			echo "...done."
+		else
+			echo "Write error!"
+		fi
+         	;;
+	"-hold -3")
+		sudo echo "30,30,30,30,30,30,30,30,30,30,30,30,30,30,30" >> /dev/ttyACM0
+		if [ "$?" == "0" ]; then
+			echo "...done."
+		else
+			echo "Write error!"
+		fi
+         	;;
+	"-hold -2")
+		sudo echo "20,20,20,20,20,20,20,20,20,20,20,20,20,20,20" >> /dev/ttyACM0
+		if [ "$?" == "0" ]; then
+			echo "...done."
+		else
+			echo "Write error!"
+		fi
+         	;;
+	"-hold -1")
+		sudo echo "10,10,10,10,10,10,10,10,10,10,10,10,10,10,10" >> /dev/ttyACM0
+		if [ "$?" == "0" ]; then
+			echo "...done."
+		else
+			echo "Write error!"
+		fi
+         	;;
+	"-init")
+		sudo echo "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0" >> /dev/ttyACM0
+		if [ "$?" == "0" ]; then
+			echo "...done."
+		else
+			echo "Write error!"
+		fi
+         	;;
 	"")
-		echo "Usage: mainarm.sh [operation]";
+		echo "Usage: mainarm.sh [operation] [level]";
 		echo "Support operations:";
-		echo "-hold";
+		echo "-hold [-1]-[-9]";
+		echo "-init";
 		;;
 	*)
 		echo "You must specify a functional operation!";
