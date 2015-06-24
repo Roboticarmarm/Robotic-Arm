7c804a1f3aa42546b7166e1d63502475b86b57b0
mopplayer <mopplayer@yahoo.com.tw>
2015-06-24 05:19:42 +0000

    Simplify arm

--- mainarm.ino
+++ mainarm.ino
@@ -1,67 +1,48 @@
 //Beta version for hand control
 
-#include <Servo.h> 
+#include <Servo.h>
+const int NUM=4;
 int incomingByte=0 ;
-int data[17]={};
+int data[NUM]={};
+int current[NUM]={};
 int count=0;
 int index=0;
-Servo th1,th2,th3,fo1,fo2,fo3,mi1,mi2,mi3,ri1,ri2,ri3,li1,li2,li3; //th=thumb,fo=forefinger,mi=middle,ri=ring,li=little
+Servo th1,th2,th3,fo1;
+int pos = 0;
 
 void setup() {
-  pinMode(22,INPUT);
-  th1.attach(23); 
-  pinMode(24,INPUT);
-  th2.attach(25);  
-  pinMode(26,INPUT);
-  th3.attach(27);  
-  pinMode(28,INPUT);
-  fo1.attach(29);  
-  pinMode(30,INPUT);
-  fo2.attach(31);  
-  pinMode(32,INPUT);
-  fo3.attach(33);  
-  pinMode(34,INPUT);
-  mi1.attach(35);  
-  pinMode(36,INPUT);
-  mi2.attach(37);  
-  pinMode(38,INPUT);
-  mi3.attach(39);  
-  pinMode(40,INPUT);
-  ri1.attach(41);  
-  pinMode(42,INPUT);
-  ri2.attach(43);  
-  pinMode(44,INPUT);
-  ri3.attach(45); 
-  pinMode(46,INPUT);
-  li1.attach(47);
-  pinMode(48,INPUT);
-  li2.attach(49);
-  pinMode(50,INPUT);
-  li3.attach(51);
+  th1.attach(2); 
+  th2.attach(3);  
+  th3.attach(4);  
+  fo1.attach(5);  
+ 
   Serial.begin(115200);
 }
 
 void loop() {
+  
   if (Serial.available()) {
     
     incomingByte = Serial.read();
 
     if (incomingByte==10)
     {
-      for(int i=0;i<17;i=i+1)
+           
+      for(int i=0;i<NUM;i=i+1)
       {
+        current[i]=data[i];
         Serial.print(data[i]);
         Serial.print(",");
       }
       Serial.println("...processing");
-      WriteServo(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10], data[11], data[12], data[13], data[14]); //write fetcing data to servo
-
-      for(int i=0;i<17;i=i+1) //init
+      WriteServo(data[0], data[1], data[2], data[3]); //write fetcing data to servo
+          
+      for(int i=0;i<NUM;i=i+1)
       {
         data[i]=0;
-      }
+      }      
       count=-1;
-      index=0;        
+      index=0;              
     }
     else if (incomingByte==44)
     { 
@@ -80,23 +61,31 @@ void loop() {
     }
     count=count+1;
   }
+  else
+  {
+    WriteServo(current[0]+30, current[1], current[2], current[3]); //write fetcing data to servo
+  }
+
+ /*   for(pos = 0; pos <= 180; pos += 1) // goes from 0 degrees to 180 degrees 
+  {                                  // in steps of 1 degree 
+    th1.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position 
+  } 
+  for(pos = 180; pos>=0; pos-=1)     // goes from 180 degrees to 0 degrees 
+  {                                
+    th1.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position 
+  }*/
 }
 
-void WriteServo(int a1, int a2, int a3, int b1, int b2, int b3, int c1, int c2 ,int c3 ,int d1, int d2 ,int d3 ,int e1, int e2 ,int e3 )
+void WriteServo(int a1, int a2, int a3, int b1)
 {
-    th1.write(a1);   
+    th1.write(a1);
+    delay(1);   
     th2.write(a2);
+    delay(1);
     th3.write(a3);
-    fo1.write(b1);   
-    fo2.write(b2);
-    fo3.write(b3);
-    mi1.write(c1);   
-    mi2.write(c2);
-    mi3.write(c3);
-    ri1.write(d1);   
-    ri2.write(d2);
-    ri3.write(d3);
-    li1.write(e1);   
-    li2.write(e2);
-    li3.write(e3);
+    delay(1);
+    fo1.write(b1);
+    delay(1);  
 }
--- mainarm.sh
+++ mainarm.sh
@@ -1,76 +1,77 @@
+#!/bin/bash
 #Serial control for ttyUSB=ttyACM0
-
+ 
 echo "Now initializing Serial port for ttyACM0..."
-sudo chmod 777 /dev/ttyACM0
+chmod 777 /dev/ttyACM0
 if [ "$?" == "0" ]; then
 	echo "Succeeded!"
 	case $1 in
-	"-hold -9")
-		sudo echo "90,90,90,90,90,90,90,90,90,90,90,90,90,90,90" >> /dev/ttyACM0
+	"-hold9")
+		sudo echo "50,99,50,10" >> /dev/ttyACM0
 		if [ "$?" == "0" ]; then
 			echo "...done."
 		else
 			echo "Write error!"
 		fi
          	;;
-	"-hold -8")
-		sudo echo "80,80,80,80,80,80,80,80,80,80,80,80,80,80,80" >> /dev/ttyACM0
+	"-hold8")
+		sudo echo "70,80,80,80" >> /dev/ttyACM0
 		if [ "$?" == "0" ]; then
 			echo "...done."
 		else
 			echo "Write error!"
 		fi
          	;;
-	"-hold -7")
-		sudo echo "70,70,70,70,70,70,70,70,70,70,70,70,70,70,70" >> /dev/ttyACM0
+	"-hold7")
+		sudo echo "60,70,70,70" >> /dev/ttyACM0
 		if [ "$?" == "0" ]; then
 			echo "...done."
 		else
 			echo "Write error!"
 		fi
          	;;
-	"-hold -6")
-		sudo echo "60,60,60,60,60,60,60,60,60,60,60,60,60,60,60" >> /dev/ttyACM0
+	"-hold6")
+		sudo echo " 50,60,60,60" >> /dev/ttyACM0
 		if [ "$?" == "0" ]; then
 			echo "...done."
 		else
 			echo "Write error!"
 		fi
          	;;
-	"-hold -5")
-		sudo echo "50,50,50,50,50,50,50,50,50,50,50,50,50,50,50" >> /dev/ttyACM0
+	"-hold5") 
+		sudo echo "50,50,50,50" >> /dev/ttyACM0
 		if [ "$?" == "0" ]; then
 			echo "...done."
 		else
 			echo "Write error!"
 		fi
          	;;
-	"-hold -4")
-		sudo echo "40,40,40,40,40,40,40,40,40,40,40,40,40,40,40" >> /dev/ttyACM0
+	"-hold4")
+		sudo echo "40,40,40,40" >> /dev/ttyACM0
 		if [ "$?" == "0" ]; then
 			echo "...done."
 		else
 			echo "Write error!"
 		fi
          	;;
-	"-hold -3")
-		sudo echo "30,30,30,30,30,30,30,30,30,30,30,30,30,30,30" >> /dev/ttyACM0
+	"-hold3")
+		sudo echo "30,30,30,30" >> /dev/ttyACM0
 		if [ "$?" == "0" ]; then
 			echo "...done."
 		else
 			echo "Write error!"
 		fi
          	;;
-	"-hold -2")
-		sudo echo "20,20,20,20,20,20,20,20,20,20,20,20,20,20,20" >> /dev/ttyACM0
+	"-hold2")
+		sudo echo "20,20,20,20" >> /dev/ttyACM0
 		if [ "$?" == "0" ]; then
 			echo "...done."
 		else
 			echo "Write error!"
 		fi
          	;;
-	"-hold -1")
-		sudo echo "10,10,10,10,10,10,10,10,10,10,10,10,10,10,10" >> /dev/ttyACM0
+	"-hold1")
+		sudo echo "10,10,10,10" >> /dev/ttyACM0
 		if [ "$?" == "0" ]; then
 			echo "...done."
 		else
@@ -78,7 +79,7 @@ if [ "$?" == "0" ]; then
 		fi
          	;;
 	"-init")
-		sudo echo "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0" >> /dev/ttyACM0
+		sudo echo "80,70,99,90" >> /dev/ttyACM0
 		if [ "$?" == "0" ]; then
 			echo "...done."
 		else
